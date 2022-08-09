54c8a21722e226ae5380e9444124e6ec2abbcd86
Petr Svestka <psvestka@deloittece.com>
2022-08-09 22:33:08 +0200

    adding index.html

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+
 const jsforce = require('jsforce');
 
 const conn = new jsforce.Connection();
