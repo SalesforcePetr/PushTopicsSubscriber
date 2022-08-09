5422bf8e6ce064aaf050bc41160adf21b76abc05
Petr Svestka <psvestka@deloittece.com>
2022-08-09 22:05:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const jsforce = require('jsforce');
+
+const conn = new jsforce.Connection();
+conn.login(`${process.env.SFDC_USERNAME}`, `${process.env.SFDC_PASSWORD}${process.env.SFDC_SECURITY_TOKEN}`, function(err, res) {
+  if (err) { 
+      return console.error(err);
+  }
+
+  console.log('Authenticated');
+  
+  conn.streaming.topic("AccountAddressUpdates").subscribe(function(message) {
+    console.log('------------------------------------------------');
+    console.log('%cEvent Type: ' + `%c${message.event.type} at ${message.event.createdDate}`, 'font-weight: bold;', 'font-weight: normal;');
+    //console.log('Object Id : ' + message.sobject.Id);
+    //console.log('Event : ' + JSON.stringify(message));
+    console.log(`%cReceived new address for Customer ID ${message.sobject.FinServ__CustomerID__c}:` + `%c\n${message.sobject.BillingStreet}\n${message.sobject.BillingPostalCode} ${message.sobject.BillingCity}\n${message.sobject.BillingCountry}`, 'font-weight: bold;', 'font-weight: bold;');
+    console.log('%cPayload: ' + `%c${JSON.stringify(message)}`, 'font-weight: bold;', 'font-weight: bold;');
+    console.table(message.sobject);
+  });
+});
